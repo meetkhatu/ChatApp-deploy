0357c0a72395382666381f06fecd698580b398bc
meetkhatu <143870423+meetkhatu@users.noreply.github.com>
2024-11-18 21:52:06 +0530

    final changes

--- SocketIO/server.js
+++ SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:5002', 'http://localhost:3000'],
+        origin: ['https://mychat-4ayr.onrender.com'],
         methods: ['GET', 'POST']
     }
 })
